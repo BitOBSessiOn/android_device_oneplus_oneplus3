6023b748bf4f1a5cc9079de2253a2e793623e96b
BitO BSessiOn <bitobsession@gmail.com>
2017-02-22 22:58:29 +0100

    Code stripped down

--- omni_oneplus3.mk
+++ omni_oneplus3.mk
@@ -7,10 +7,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_PACKAGES += \
-	charger_res_images \
-	charger
-
 # Set by init module based on ro.boot.rf_version argument from bootloader
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.device ro.product.model
 
